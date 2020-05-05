ff4f8f4002390b2944722b268fd0a06d1f607cc3
ckinzel <40035023+ckinzel@users.noreply.github.com>
2020-05-05 16:04:48 -0700

    Program c and e

---
+++ c_and_e.py
@@ -0,0 +1,31 @@
+
+
+def m_time():
+    
+     time = input('Enter your time: ')
+     y = input('am or pm: ')
+    
+     word = time.split(":")
+     hour = int(word[0]);
+     if (y == 'pm'):
+         hour = hour + 12
+
+     print("Military time: " + str(hour) + ":" + word[1])
+
+m_time()
+
+def password():
+     count = 0
+     pw = input('Enter your password: ')
+     # 33 - 63
+     for element in range(0, len(pw)): 
+        if (ord(pw[element]) > 32 and ord(pw[element]) < 64):
+            count = count + 1
+     if (len(pw) < 7):
+         print("No good bro")
+     elif (count < 2):
+         print("No good bro")
+     else:
+         print("password is good")
+         
+password()
