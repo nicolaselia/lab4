131d53ebd48c41e21ac90ea8579947ef7ad4fd9b
Nick Elia <006270546@coyote.csusb.edu>
2020-05-05 16:54:36 -0700

    First merge

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 # AlphaOne Lab4
 #
 
+# Done by Nick
+def reverse_string(string):
+	return string[::-1]
+
+
+def is_prime(numb):
+	if numb > 1:
+	    for i in range(2, numb):
+	        if (numb % i) == 0:
+	            return False
+	    else:
+	        return True
+
+string = input("Input a string")
+print("The reversed string is", reverse_string(string))
+
+numb = int(input("Input a number"))
+if(is_prime(numb)):
+	print("The number", numb, "is prime.")
+else:
+	print("The number", numb, "is not prime.")
