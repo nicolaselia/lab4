e1c5e22a3a59c5e299673146298309402b4dc5cf
ckinzel <40035023+ckinzel@users.noreply.github.com>
2020-05-05 17:25:19 -0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-<<<<<<< HEAD
+
 # Charles Kinzel
+# Nick Elia
+# David Monge
 # Lab 4
 # CSE 408
 # Team AlphaOne
 
 
+# Charles Code
 def m_time():
     
      time = input('Enter your time: ')
@@ -35,10 +38,6 @@ def password():
          
 password()
 
-=======
-#
-# AlphaOne Lab4
-#
 
 # Done by Nick
 def reverse_string(string):
@@ -61,41 +60,5 @@ if(is_prime(numb)):
 	print("The number", numb, "is prime.")
 else:
 	print("The number", numb, "is not prime.")
->>>>>>> 131d53ebd48c41e21ac90ea8579947ef7ad4fd9b
-
-# Charles Kinzel
-# Lab 4
-# CSE 408
-# Team AlphaOne
-
-def m_time():
-    
-     time = input('Enter your time: ')
-     y = input('am or pm: ')
-    
-     word = time.split(":")
-     hour = int(word[0]);
-     if (y == 'pm'):
-         hour = hour + 12
-
-     print("Military time: " + str(hour) + ":" + word[1])
-
-m_time()
-
-def password():
-     count = 0
-     pw = input('Enter your password: ')
-     # 33 - 63
-     for element in range(0, len(pw)): 
-        if (ord(pw[element]) > 32 and ord(pw[element]) < 64):
-            count = count + 1
-     if (len(pw) < 7):
-         print("No good bro")
-     elif (count < 2):
-         print("No good bro")
-     else:
-         print("password is good")
-         
-password()
 
 
