5a26171db4861a80b84d2d4620ff8c83f08f9382
David <davidmonge@Davids-MacBook-Air-2.local>
2020-05-05 17:31:01 -0700

    Davids part

--- main.py
+++ main.py
@@ -61,4 +61,15 @@ if(is_prime(numb)):
 else:
 	print("The number", numb, "is not prime.")
 
+# Done by David 
+
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return(fibo(n-1)+fibo(n-2))
+
+nNum = 10
+for i in range(nNum):
+    print(fibo(i))
 
